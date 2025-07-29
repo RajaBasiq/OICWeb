f92a8822f5bc609332f5ceee52fc4bc90a6e82f3
Raja Basiq Ashraf <bashraf@ssidecisions.com>
2025-07-29 19:29:25 +0500

    edit screen added

--- src/app/component/student/student.component.ts
+++ src/app/component/student/student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { StudentService } from 'src/app/service/student.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-student',
@@ -10,8 +11,10 @@ import { StudentService } from 'src/app/service/student.service';
 export class StudentComponent implements OnInit {
   
   studentForm: FormGroup;
+  isEditMode=false;
+  userId: string | null = null;
 
-  constructor(private fb: FormBuilder,  private studentService: StudentService
+  constructor(private fb: FormBuilder,  private studentService: StudentService,private route: ActivatedRoute
   ) 
   {
     this.studentForm = this.fb.group({
@@ -39,44 +42,75 @@ export class StudentComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("student-form");
+    this.userId=this.route.snapshot.paramMap.get('id') ;
+    this.isEditMode= !!this.userId;
+    if(this.isEditMode)
+      {
+        this.studentService.get(parseInt(this.userId)).subscribe(student => {
+          this.studentForm.patchValue({
+            student:student.student,
+            guardian:student.guardian,
+            availHostelFacility: student.availHostelFacility,
+            availLibraryFacility: student.availLibraryFacility,  
+          });
+          this.patchEducationArray(student.educations);
+        });
+      }
     }
-    createEducation(): FormGroup {
-      return this.fb.group({
-        Name: ['', Validators.required],
-        StartingYear: ['', Validators.required],
-        EndingYear: [''],
-        Institute: ['', Validators.required],
-        ResultPercentage: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
-        Ongoing: [false]
-      });
-    }
+  createEducation(): FormGroup {
+    return this.fb.group({
+      Name: ['', Validators.required],
+      StartingYear: ['', Validators.required],
+      EndingYear: [''],
+      Institute: ['', Validators.required],
+      ResultPercentage: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+      Ongoing: [false]
+    });
+  }
+  createEducationGroup(data: any): FormGroup {
+    return this.fb.group({
+      Name: [data.name,],
+      StartingYear: [data.startingYear],
+      EndingYear: [data.endingYear],
+      Institute: [data.institute],
+      ResultPercentage: [data.resultPercentage],
+      Ongoing: [data.ongoing]
+    });
+  }
+  patchEducationArray(educationList: any[]) {
+    this.educations.clear(); // Clear existing controls first
+
+    educationList.forEach(edu => {
+      this.educations.push(this.createEducationGroup(edu));
+    });
+  }
       // Get Education History form array
   get educations(): FormArray {
     return this.studentForm.get('educations') as FormArray;
   }
     // Add a new education record
-    addEducation() {
-      this.educations.push(this.createEducation());
-    }
+  addEducation() {
+    this.educations.push(this.createEducation());
+  }
   // Remove an education record
   removeEducation(index: number) {
     this.educations.removeAt(index);
   }
-    // Submit the form
-    onSubmit() {
-      debugger;
-      console.log('Form submitted:', this.studentForm.value);
-      if (this.studentForm.valid) {
-        this.studentService.postStudent(this.studentForm.value).subscribe(
-          response => {
-            console.log('Student information saved successfully', response);
-          },
-          error => {
-            console.error('Error saving student information', error);
-          }
-        );
-      } else {
-        console.log('Form is invalid');
-      }
-    }  
+  // Submit the form
+  onSubmit() {
+    debugger;
+    console.log('Form submitted:', this.studentForm.value);
+    if (this.studentForm.valid) {
+      this.studentService.postStudent(this.studentForm.value).subscribe(
+        response => {
+          console.log('Student information saved successfully', response);
+        },
+        error => {
+          console.error('Error saving student information', error);
+        }
+      );
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
